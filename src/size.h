f652bc2cc08d415b3e77638d774d7fd0e9f4b81f
Marcin Kurdziel <marcin.kurdziel97@gmail.com>
2019-05-02 21:23:06 +0200

    add size.h

---
+++ src/size.h
@@ -0,0 +1,19 @@
+#ifndef PARALLEL_BENCHMARK_SIZE_H
+#define PARALLEL_BENCHMARK_SIZE_H
+
+// task 1 and 3
+#define N1 1000
+#define N2 10000
+#define N3 100000
+#define N4 1000000
+#define N5 10000000
+#define N6 100000000
+#define N7 1000000000
+
+// task 2
+#define F1 10
+#define F2 100
+#define F3 1000
+#define F4 10000
+
+#endif // !1PARALLEL_BENCHMARK_SIZE_H
