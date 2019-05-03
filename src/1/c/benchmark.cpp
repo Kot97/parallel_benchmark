f674e34fb2f659ffb8345c82081966d7d65549df
Marcin Kurdziel <marcin.kurdziel97@gmail.com>
2019-05-03 11:43:40 +0200

    change in benchmark.cpp

--- src/1/c/benchmark.cpp
+++ src/1/c/benchmark.cpp
@@ -1,6 +1,7 @@
 #include "../write.hpp"
 #include "pthread_.h"
 #include "openmp_.h"
+#include "serial_.h"
 
 int main(int argc, char const *argv[])
 {
@@ -36,19 +37,20 @@ int main(int argc, char const *argv[])
         step = 100;
     }
 
-
     printf("Benchmarking element-wise vector multiplication in C...\n\n");
     benchmark_time_t benchmark_start = get_time();
 
     unsigned long size = (max - min) / step + 1;
     
-    unsigned long *values = (unsigned long*)malloc(sizeof(unsigned long)*size);
+    unsigned long *task_size = (unsigned long*)malloc(sizeof(unsigned long)*size);
+    benchmark_time_t *times_serial = (benchmark_time_t*)malloc(sizeof(benchmark_time_t)*size);
     benchmark_time_t *times_pthread = (benchmark_time_t*)malloc(sizeof(benchmark_time_t)*size);
     benchmark_time_t *times_openmp = (benchmark_time_t*)malloc(sizeof(benchmark_time_t)*size);
     benchmark_time_t *times_openmp_for = (benchmark_time_t*)malloc(sizeof(benchmark_time_t)*size);
     benchmark_time_t *times_openmp_for_simd = (benchmark_time_t*)malloc(sizeof(benchmark_time_t)*size);
 
-    unsigned long *temp_values= values;
+    unsigned long *temp_task_size = task_size;
+    benchmark_time_t *temp_times_serial = times_serial;
     benchmark_time_t *temp_times_pthread = times_pthread;
     benchmark_time_t *temp_times_openmp = times_openmp;
     benchmark_time_t *temp_times_openmp_for = times_openmp_for;
@@ -59,11 +61,16 @@ int main(int argc, char const *argv[])
     double *result, *a, *b;
     for(unsigned long i = min; i < max; i+=step)
     {
-        *(temp_values++) = i;
+        *(temp_task_size++) = i;
         result = (double*)malloc(sizeof(double)*i);
         a = (double*)malloc(sizeof(double)*i);
         b = (double*)malloc(sizeof(double)*i);
         
+        start = get_time();
+        c1_serial(result, a, b, i);
+        end = get_time();
+        *(temp_times_serial++) = (end - start);
+
         start = get_time();
         c1_pthread(result, a, b, i, num_threads);
         end = get_time();
@@ -92,10 +99,16 @@ int main(int argc, char const *argv[])
     const char* openmp_for_filename = "../../data/1/c/openmp_parallel_for.txt";
     const char* openmp_for_simd_filename = "../../data/1/c/openmp_parallel_for_simd.txt";
 
+    write(pthread_filename, task_size, times_pthread, size);
+    write(openmp_filename, task_size, times_openmp, size);
+    write(openmp_for_filename, task_size, times_openmp_for, size);
+    write(openmp_for_simd_filename, task_size, times_openmp_for_simd, size);
+
     auto benchmark_end = std::chrono::steady_clock::now().time_since_epoch().count();
     
     printf("Benchmark took %lf s", (benchmark_end - benchmark_start)/1000);
 
-    free(values); free(times_pthread); free(times_openmp); free(times_openmp_for); free(times_openmp_for_simd);
+    free(task_size); free(times_serial); free(times_pthread); free(times_openmp); 
+    free(times_openmp_for); free(times_openmp_for_simd);
     return 0;
 }
