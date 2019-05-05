758967fb478b728aaac559a7692a80a5958d2e46
Marcin Kurdziel <marcin.kurdziel97@gmail.com>
2019-05-05 20:40:13 +0200

    serial complete

---
+++ src/2/c/serial_.h
@@ -0,0 +1,44 @@
+#ifndef PARALLEL_BENCHMARK_SERIAL_2_H
+#define PARALLEL_BENCHMARK_SERIAL_2_H
+
+#include <stdlib.h>
+
+unsigned long c2_serial_rec(long n)
+{
+    if(n <= 2)  return 1;
+    return c2_serial_rec(n-1) + c2_serial_rec(n-2);
+}
+
+#define DCACHE_SIZE 50
+
+long dictionary[DCACHE_SIZE] = { 0 };
+
+long c2_serial_rec_dict(long n)
+{
+    if (n <= 2)
+        return 1;
+    if (n < DCACHE_SIZE) {
+        if (dictionary[n] != 0)
+            return dictionary[n];
+        else
+            return dictionary[n] = c2_serial_rec_dict(n - 1) + c2_serial_rec_dict(n - 2);
+    } else {
+        return c2_serial_rec_dict(n - 1) + c2_serial_rec_dict(n - 2);
+    }
+}
+
+unsigned long c2_serial_iter(long n)
+{
+    if(n <= 2) return 1;
+    unsigned long a = 1, b = 1, temp;
+    while((n--) > 2)
+    {
+        temp = a;
+        a = b;
+        b += temp;
+    }
+    return b;
+}
+
+
+#endif // !1PARALLEL_BENCHMARK_SERIAL_2_H
