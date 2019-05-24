529428d6a725f43b8a54504b172851487ea9caa0
kot97 <marcin.kurdziel97@gmail.com>
2019-05-24 20:56:19 +0200

    mpi 1 tests complete

--- test/mpi_test_c_vector_mult.hpp
+++ test/mpi_test_c_vector_mult.hpp
@@ -1,22 +1,24 @@
-#ifndef PARALLEL_BENCHMARK_MPI_CPP_VECTOR_MULT_TEST_HPP
-#define PARALLEL_BENCHMARK_MPI_CPP_VECTOR_MULT_TEST_HPP
+#ifndef PARALLEL_BENCHMARK_MPI_C_VECTOR_MULT_TEST_HPP
+#define PARALLEL_BENCHMARK_MPI_C_VECTOR_MULT_TEST_HPP
 
 #include <gtest/gtest.h>
 #include <stdlib.h>
 #include <tuple>
 #include <vector>
-#include "../src/1/cpp/mpi_.hpp"
+#include "../src/1/c/mpi_.h"
 
-class MpiVectorMultNotSetTest : public ::testing::TestWithParam<unsigned long>
+class CMpiVectorMultNotSetTest : public ::testing::TestWithParam<unsigned long>
 {
 public:
     double *res, *a, *b;
     unsigned long size = GetParam();
-    mpi::communicator comm;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank;
 
     void SetUp() override
     {
-        if(comm.rank() != 0) return;
+        MPI_Comm_rank(comm, &rank);
+        if(rank != 0) return;
         res = (double*)malloc(sizeof(double) * size);
         a = (double*)malloc(sizeof(double) * size);
         b = (double*)malloc(sizeof(double) * size);
@@ -24,30 +26,32 @@ public:
 
     void TearDown() override
     {
-        if(comm.rank() != 0) return;
+        if(rank != 0) return;
         free(res); free(a); free(b);
     }
 };
-INSTANTIATE_TEST_SUITE_P(SmallSize, MpiVectorMultNotSetTest, ::testing::Range(2ul, 100ul));
-INSTANTIATE_TEST_SUITE_P(MediumSize, MpiVectorMultNotSetTest, 
+INSTANTIATE_TEST_SUITE_P(SmallSize, CMpiVectorMultNotSetTest, ::testing::Range(2ul, 100ul));
+INSTANTIATE_TEST_SUITE_P(MediumSize, CMpiVectorMultNotSetTest, 
 ::testing::Values(
     1000, 1001, 1003, 4578, 11203, 76711
 ));
-INSTANTIATE_TEST_SUITE_P(LargeSize, MpiVectorMultNotSetTest, 
+INSTANTIATE_TEST_SUITE_P(LargeSize, CMpiVectorMultNotSetTest, 
 ::testing::Values(
     100000, 100002, 100005, 200123, 340028, 1000000, 1000008
 ));
 
-class MpiVectorMultSetTest : public ::testing::TestWithParam<std::tuple<unsigned long, double, double, double>>
+class CMpiVectorMultSetTest : public ::testing::TestWithParam<std::tuple<unsigned long, double, double, double>>
 {
 public:
     double *res, *a, *b;
     unsigned long size = std::get<0>(GetParam());
-    mpi::communicator comm;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank;
 
     void SetUp() override
     {
-        if(comm.rank() != 0) return;
+        MPI_Comm_rank(comm, &rank);
+        if(rank != 0) return;
         res = (double*)malloc(sizeof(double) * size);
         a = (double*)malloc(sizeof(double) * size);
         b = (double*)malloc(sizeof(double) * size);
@@ -64,7 +68,7 @@ public:
 
     void TearDown() override
     {
-        if(comm.rank() != 0) return;
+        if(rank != 0) return;
         double _res = std::get<3>(GetParam());
         double *res_help = res;
         for(unsigned long i = 0; i < size; ++i, ++res_help)
@@ -72,7 +76,7 @@ public:
         free(res); free(a); free(b);
     }
 };
-INSTANTIATE_TEST_SUITE_P(SmallSizeInt, MpiVectorMultSetTest,
+INSTANTIATE_TEST_SUITE_P(SmallSizeInt, CMpiVectorMultSetTest,
 ::testing::Values(
     std::make_tuple(4, 2, 3, 6),
     std::make_tuple(19, 2, 3, 6),
@@ -80,7 +84,7 @@ INSTANTIATE_TEST_SUITE_P(SmallSizeInt, MpiVectorMultSetTest,
     std::make_tuple(93, 2, 3, 6),
     std::make_tuple(199, 2, 3, 6)
 ));
-INSTANTIATE_TEST_SUITE_P(SmallSizeDouble, MpiVectorMultSetTest,
+INSTANTIATE_TEST_SUITE_P(SmallSizeDouble, CMpiVectorMultSetTest,
 ::testing::Values(
     std::make_tuple(4, 2.2, 3.3, 7.26),
     std::make_tuple(19, 2.2, 3.3, 7.26),
@@ -88,7 +92,7 @@ INSTANTIATE_TEST_SUITE_P(SmallSizeDouble, MpiVectorMultSetTest,
     std::make_tuple(93, 2.2, 3.3, 7.26),
     std::make_tuple(199, 2.2, 3.3, 7.26)
 ));
-INSTANTIATE_TEST_SUITE_P(LargeSizeInt, MpiVectorMultSetTest,
+INSTANTIATE_TEST_SUITE_P(LargeSizeInt, CMpiVectorMultSetTest,
 ::testing::Values(
     std::make_tuple(1004, 2, 3, 6),
     std::make_tuple(15698, 2, 3, 6),
@@ -96,7 +100,7 @@ INSTANTIATE_TEST_SUITE_P(LargeSizeInt, MpiVectorMultSetTest,
     std::make_tuple(234605, 2, 3, 6),
     std::make_tuple(1123544, 2, 3, 6)
 ));
-INSTANTIATE_TEST_SUITE_P(LargeSizeDouble, MpiVectorMultSetTest,
+INSTANTIATE_TEST_SUITE_P(LargeSizeDouble, CMpiVectorMultSetTest,
 ::testing::Values(
     std::make_tuple(1004, 2.2, 3.3, 7.26),
     std::make_tuple(15698, 2.2, 3.3, 7.26),
@@ -105,18 +109,20 @@ INSTANTIATE_TEST_SUITE_P(LargeSizeDouble, MpiVectorMultSetTest,
     std::make_tuple(1123544, 2.2, 3.3, 7.26)
 ));
 
-class MpiVectorMultTest : public ::testing::TestWithParam<
+class CMpiVectorMultTest : public ::testing::TestWithParam<
         std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>>
 {
 public:
     double *res;
     const double *a = std::get<0>(GetParam()).data(), *b = std::get<1>(GetParam()).data(); 
     unsigned int size = std::get<0>(GetParam()).size();
-    mpi::communicator comm;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    int rank;
 
     void SetUp() override
     {
-        if(comm.rank() != 0) return;
+        MPI_Comm_rank(comm, &rank);
+        if(rank != 0) return;
         unsigned size2 = std::get<1>(GetParam()).size(), size3 = std::get<2>(GetParam()).size();
         ASSERT_EQ(size, size2) << "Element-wise vector multiplication requires vector of equal length!";
         ASSERT_EQ(size2, size3) << "Element-wise vector multiplication requires vector of equal length!";
@@ -125,7 +131,7 @@ public:
 
     void TearDown() override
     {
-        if(comm.rank() != 0) return;
+        if(rank != 0) return;
         const double* _res = std::get<2>(GetParam()).data();
         double *res_help = res;
         for(unsigned long i = 0; i < size; ++i, ++res_help, ++_res)
@@ -133,37 +139,36 @@ public:
         free(res);
     }
 };
-INSTANTIATE_TEST_SUITE_P(Int, MpiVectorMultTest,
+INSTANTIATE_TEST_SUITE_P(Int, CMpiVectorMultTest,
 ::testing::Values(
     std::make_tuple(std::vector<double>{1,2,3,4}, std::vector<double>{1,2,3,4}, std::vector<double>{1,4,9,16}),
     std::make_tuple(std::vector<double>{6,7,8,9,10}, std::vector<double>{6,7,8,9,10}, std::vector<double>{36,49,64,81,100}),
     std::make_tuple(std::vector<double>{1001,1002,1003,10019}, std::vector<double>{1,1,1,1}, std::vector<double>{1001,1002,1003,10019})
 ));
-INSTANTIATE_TEST_SUITE_P(Double, MpiVectorMultTest,
+INSTANTIATE_TEST_SUITE_P(Double, CMpiVectorMultTest,
 ::testing::Values(
     std::make_tuple(std::vector<double>{1.1,2.2,3.3,4.4}, std::vector<double>{1,2,3,4}, std::vector<double>{1.1,4.4,9.9,17.6}),
     std::make_tuple(std::vector<double>{6.1,7.1,8.1,9.1,10.1}, std::vector<double>{6,7,8,9,10}, std::vector<double>{36.6,49.7,64.8,81.9,101}),
     std::make_tuple(std::vector<double>{1001,1002,1003,10019}, std::vector<double>{1,1.0,1,1.0}, std::vector<double>{1001,1002.0,1003,10019})
 ));
 
-#define MPI_TEST_NULL_INPUT(test_name, function_name)\
+#define C_MPI_TEST_NULL_INPUT(test_name, function_name)\
 TEST(NullInput, test_name)\
 {\
-    mpi::communicator comm;\
-    function_name(comm, NULL, NULL, NULL, 0);\
-    function_name(comm, nullptr, nullptr, nullptr, 0);\
+    function_name(MPI_COMM_WORLD, NULL, NULL, NULL, 0);\
+    function_name(MPI_COMM_WORLD, nullptr, nullptr, nullptr, 0);\
 }
 
-#define MPI_TEST_VECTOR_MULT(test_name, function_name)\
-TEST_P(MpiVectorMultSetTest, test_name)\
+#define C_MPI_TEST_VECTOR_MULT(test_name, function_name)\
+TEST_P(CMpiVectorMultSetTest, test_name)\
 {\
     function_name(comm, res, a, b, size);\
 }\
-TEST_P(MpiVectorMultNotSetTest, test_name)\
+TEST_P(CMpiVectorMultNotSetTest, test_name)\
 {\
     function_name(comm, res, a, b, size);\
 }\
-TEST_P(MpiVectorMultTest, test_name)\
+TEST_P(CMpiVectorMultTest, test_name)\
 {\
     function_name(comm, res, a, b, size);\
 }
--- test/mpi_test_cpp.cpp
+++ test/mpi_test_cpp.cpp
@@ -1,10 +1,14 @@
-#include "mpi_test_cpp_vector_mult.hpp"
 #include "mpi_environment.hpp"
+#include "mpi_test_cpp_vector_mult.hpp"
+#include "mpi_test_c_vector_mult.hpp"
 
 MPI_TEST_NULL_INPUT(BoostMpi, cpp1_mpi)
 MPI_TEST_VECTOR_MULT(BoostMpi, cpp1_mpi)
 
-// MPI_TEST_FIBONACCI(BoostMpi, cpp2_mpi_rec)
+C_MPI_TEST_NULL_INPUT(OpenMpi, c1_mpi)
+C_MPI_TEST_VECTOR_MULT(OpenMpi, c1_mpi)
+
+// mpic++ -o mpi_cpp.test mpi_test_cpp.cpp -lgtest -lpthread -lboost_mpi
 
 int main(int argc, char **argv)
 {
