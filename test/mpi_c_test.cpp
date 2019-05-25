c480175108c41ec5252758d59a269eeef8af45b6
kot97 <marcin.kurdziel97@gmail.com>
2019-05-25 10:26:12 +0200

    3 tests complete

--- test/mpi_c_test.cpp
+++ test/mpi_c_test.cpp
@@ -1,8 +1,10 @@
 #include "mpi_environment.hpp"
 #include "mpi_test_c_vector_mult.hpp"
+#include "mpi_test_c_search.hpp"
 
 C_MPI_TEST_NULL_INPUT(OpenMpi, c1_mpi)
 C_MPI_TEST_VECTOR_MULT(OpenMpi, c1_mpi)
+C_MPI_TEST_SEARCH(OpenMpi, c3_mpi)
 
 // mpic++ -o mpi_c.test mpi_c_test.cpp -lgtest -lpthread
 // mpirun -n N mpi_c.test
--- test/mpi_cpp_test.cpp
+++ test/mpi_cpp_test.cpp
@@ -1,8 +1,10 @@
 #include "mpi_environment.hpp"
 #include "mpi_test_cpp_vector_mult.hpp"
+#include "mpi_test_cpp_search.hpp"
 
 MPI_TEST_NULL_INPUT(BoostMpi, cpp1_mpi)
 MPI_TEST_VECTOR_MULT(BoostMpi, cpp1_mpi)
+MPI_TEST_SEARCH(BoostMpi, cpp3_mpi)
 
 // mpic++ -o mpi_cpp.test mpi_cpp_test.cpp -lgtest -lpthread -lboost_mpi
 // mpirun -n N mpi_cpp.test
