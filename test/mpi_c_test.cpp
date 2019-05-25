32a9b652352f0c5531687a7f0b976870334a9c1b
kot97 <marcin.kurdziel97@gmail.com>
2019-05-25 09:02:36 +0200

    divide test into c and cpp

--- test/mpi_c_test.cpp
+++ test/mpi_c_test.cpp
@@ -1,14 +1,11 @@
 #include "mpi_environment.hpp"
-#include "mpi_test_cpp_vector_mult.hpp"
 #include "mpi_test_c_vector_mult.hpp"
-
-MPI_TEST_NULL_INPUT(BoostMpi, cpp1_mpi)
-MPI_TEST_VECTOR_MULT(BoostMpi, cpp1_mpi)
 
 C_MPI_TEST_NULL_INPUT(OpenMpi, c1_mpi)
 C_MPI_TEST_VECTOR_MULT(OpenMpi, c1_mpi)
 
-// mpic++ -o mpi_cpp.test mpi_test_cpp.cpp -lgtest -lpthread -lboost_mpi
+// mpic++ -o mpi_c.test mpi_c_test.cpp -lgtest -lpthread
+// mpirun -n N mpi_c.test
 
 int main(int argc, char **argv)
 {
---
+++ test/mpi_cpp_test.cpp
@@ -0,0 +1,15 @@
+#include "mpi_environment.hpp"
+#include "mpi_test_cpp_vector_mult.hpp"
+
+MPI_TEST_NULL_INPUT(BoostMpi, cpp1_mpi)
+MPI_TEST_VECTOR_MULT(BoostMpi, cpp1_mpi)
+
+// mpic++ -o mpi_cpp.test mpi_cpp_test.cpp -lgtest -lpthread -lboost_mpi
+// mpirun -n N mpi_cpp.test
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    testing::AddGlobalTestEnvironment(new MPIEnvironment);
+    return RUN_ALL_TESTS();
+}
